c2f1a799d859496fbb2a5ed072b1307092f5ef41
AnDeR <AnDeR@192.168.0.14>
2017-02-02 13:01:48 +0100

    A

--- classicmodels/src/Modelo_vista/ListarEmpleadosPorOficina.java
+++ classicmodels/src/Modelo_vista/ListarEmpleadosPorOficina.java
@@ -2,18 +2,40 @@ package Modelo_vista;
 
 import java.awt.BorderLayout;
 import java.awt.FlowLayout;
+import java.util.ArrayList;
+import java.util.List;
 
+import javax.swing.DefaultComboBoxModel;
 import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.table.DefaultTableModel;
+
+
+import modeloDB_DAO.EmployeesDAO;
+import modeloDB_DAO.OfficesDAO;
+
+import modeloDB_DTO.EmployeesDTO;
+import modeloDB_DTO.OfficesDTO;
+
 import javax.swing.JScrollPane;
 import javax.swing.JComboBox;
 import javax.swing.JLabel;
+import javax.swing.JTable;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class ListarEmpleadosPorOficina extends JDialog {
 
 	private final JPanel contentPanel = new JPanel();
+	private JComboBox<String> comboBox;
+	private JScrollPane scrollPane;
+	private ArrayList<OfficesDTO> oficinas;
+	private OfficesDAO ofi = new OfficesDAO();
+	private ArrayList<EmployeesDTO> empleados;
+	private EmployeesDAO emp = new EmployeesDAO();
+	private JTable table;
 
 	
 
@@ -27,12 +49,23 @@ public class ListarEmpleadosPorOficina extends JDialog {
 		getContentPane().add(contentPanel, BorderLayout.CENTER);
 		contentPanel.setLayout(null);
 		{
-			JScrollPane scrollPane = new JScrollPane();
+			scrollPane = new JScrollPane();
 			scrollPane.setBounds(33, 174, 703, 333);
 			contentPanel.add(scrollPane);
+			
+			table = new JTable();
+			table.setModel(new DefaultTableModel(
+				new Object[][] {
+				},
+				new String[] {
+					"EmployeeNumber", "LastName", "FirstName", "OfficeCode", "ReportsTo", "JobTitle"
+				}
+			));
+			table.getColumnModel().getColumn(0).setPreferredWidth(99);
+			scrollPane.setViewportView(table);
 		}
 		
-		JComboBox comboBox = new JComboBox();
+		comboBox = new JComboBox<String>();
 		comboBox.setBounds(360, 86, 341, 22);
 		contentPanel.add(comboBox);
 		
@@ -44,10 +77,15 @@ public class ListarEmpleadosPorOficina extends JDialog {
 			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
 			getContentPane().add(buttonPane, BorderLayout.SOUTH);
 			{
-				JButton okButton = new JButton("OK");
-				okButton.setActionCommand("OK");
-				buttonPane.add(okButton);
-				getRootPane().setDefaultButton(okButton);
+				JButton btBuscar = new JButton("Buscar");
+				btBuscar.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent arg0) {
+						cargarTabla();
+					}
+				});
+				btBuscar.setActionCommand("OK");
+				buttonPane.add(btBuscar);
+				getRootPane().setDefaultButton(btBuscar);
 			}
 			{
 				JButton cancelButton = new JButton("Cancel");
@@ -56,5 +94,38 @@ public class ListarEmpleadosPorOficina extends JDialog {
 			}
 		}
 		this.setVisible(true);
+		cargarOficinas();
+	}
+	private void cargarOficinas() {
+		OfficesDAO OffiDAO = new OfficesDAO();
+		List<OfficesDTO> offis = OffiDAO.listarTodos();
+		String[] Oficinas = new String[offis.size()];
+		for(int i=0; i<offis.size(); i++) {
+			OfficesDTO dpt = offis.get(i);
+			Oficinas[i] = dpt.getOfficeCode();
+		}
+		comboBox.setModel(new DefaultComboBoxModel<String>(Oficinas));
+	}
+	private void cargarTabla() {
+		
+	
+
+		
+		empleados = emp.listarTodosOfi(comboBox.getSelectedItem().toString());
+		
+		
+		DefaultTableModel modelo = (DefaultTableModel) table.getModel();
+		int numcols = modelo.getColumnCount();
+		for (EmployeesDTO emp : empleados) {
+			Object[] fila = new Object[numcols];
+			fila[0] = emp.getEmployeeNumber();
+			fila[1] = emp.getLastName();
+			fila[2] = emp.getFirstName();
+			fila[3] = emp.getOfficeCode();
+			fila[4] = emp.getReportsTo();
+			fila[5] = emp.getJobTitle();
+
+			modelo.addRow(fila);
+		}
 	}
 }
---
+++ classicmodels/src/modeloDB_DAO/OfficesDAO.java
@@ -0,0 +1,127 @@
+package modeloDB_DAO;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import conexion.ConexionSQL;
+import modeloDB_DTO.OfficesDTO;
+
+public class OfficesDAO implements Patron_DAO <OfficesDTO>{
+	
+	private static final String SQL_INSERT = "INSERT INTO employees (employeeNumber, lastName, firstName, extension, email, officeCode, reportsTo, jobTitle) VALUES(?,?,?,?,?,?,?,?)";
+	private static final String SQL_DELETE = "DELETE FROM employees WHERE employeeNumber = ?";
+	private static final String SQL_UPDATE = "UPDATE employees SET lastName = ?,firstName = ?, extension = ?, email = ?, officeCode = ?, reportsTo = ?, jobTitle = ? WHERE employeeNumber = ? ";
+	private static final String SQL_FIND = "SELECT * FROM employees WHERE employeeNumber = ?";
+	private static final String SQL_FINDALL = "SELECT * FROM employees";
+
+	private ConexionSQL con = ConexionSQL.getInstance();
+	
+	public boolean insertar(OfficesDTO dto){
+		PreparedStatement ps = null;
+		try{
+			ps =con.getCon().prepareStatement(SQL_INSERT);
+			ps.setString(1, dto.getOfficeCode());
+			ps.setString(2, dto.getCity());
+			ps.setString(3, dto.getPhone());
+			ps.setString(4, dto.getAddressLine1());
+			ps.setString(5, dto.getAddressLine2());
+			ps.setString(6, dto.getState());
+			ps.setString(7, dto.getCountry());
+			ps.setString(8, dto.getPostalCode());
+			ps.setString(9, dto.getTerritory());
+			
+			if(ps.executeUpdate()>0) return true;
+			
+		}catch (SQLException e) {
+			e.printStackTrace();
+		}finally{
+			try{
+				if (ps!=null) ps.close();
+			}catch (Exception e2) {
+				e2.printStackTrace();
+				
+			}
+		}
+		return false;
+	}
+	public boolean borrar(Object pk){
+		PreparedStatement ps = null;
+		try{
+			ps = con.getCon().prepareStatement(SQL_DELETE);
+			ps.setInt(1, (int)pk);
+			
+			if (ps.executeUpdate()>0) return true;
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (ps!=null) ps.close();
+			} catch (Exception e3) {
+				e3.printStackTrace();
+			}
+		}
+		return false;
+		
+	}
+	public boolean actualizar(OfficesDTO t){
+		PreparedStatement ps = null;
+		try{
+			ps = con.getCon().prepareStatement(SQL_UPDATE);
+			ps.setString(1, t.getOfficeCode());
+			ps.setString(2, t.getCity());
+			ps.setString(3, t.getPhone());
+			ps.setString(4, t.getAddressLine1());
+			ps.setString(5, t.getAddressLine2());
+			ps.setString(6, t.getState());
+			ps.setString(7, t.getCountry());
+			ps.setString(8, t.getPostalCode());
+			ps.setString(9, t.getTerritory());
+			
+			if (ps.executeUpdate()>0) return true;
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (ps!=null) ps.close();
+			} catch (Exception e4) {
+				e4.printStackTrace();
+			}
+		}
+		return false;
+	}
+	public OfficesDTO buscar(Object pk){
+		OfficesDTO Offi = null;
+		try{
+			PreparedStatement ps = con.getCon().prepareStatement(SQL_FIND);
+			
+			ps.setString(1,(String)pk);
+			ResultSet rs = ps.executeQuery();
+			if (rs.next()==true){
+				Offi = new OfficesDTO(rs.getString("officeCode"), rs.getString("city"), rs.getString("phone"), rs.getString("addressLine1"), rs.getString("addressLine2") , rs.getString("state") ,rs.getString("country") ,rs.getString("postalCode"),rs.getString("territory"));
+			}	
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return Offi;
+	}
+	public ArrayList<OfficesDTO> listarTodos() {
+		ArrayList<OfficesDTO> lista = new ArrayList<OfficesDTO>();
+		try {
+			PreparedStatement ps = con.getCon().prepareStatement(SQL_FINDALL);
+			ResultSet rs = ps.executeQuery();
+			while (rs.next()) {
+				OfficesDTO Offi = new OfficesDTO(rs.getString("officeCode"), rs.getString("city"), rs.getString("phone"), rs.getString("addressLine1"), rs.getString("addressLine2") , rs.getString("state") ,rs.getString("country") ,rs.getString("postalCode"),rs.getString("territory"));
+				lista.add(Offi);
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return lista;
+	}
+	
+	
+}
